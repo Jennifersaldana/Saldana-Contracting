803a15f6ad6238a60d7dc1ed9567c233cf59063a
Jennifer Saldana <jsalda4@lsu.edu>
2024-10-06 00:47:31 -0500

    changes

--- main.js
+++ main.js
@@ -1,17 +1,43 @@
-var navLinks = document.getElementById("navLinks");
+document.addEventListener('DOMContentLoaded', function () {
+    // Your code goes here
 
-function showMenu(){
-     navLinks.style.right = "0";
-}
-function hideMenu(){
-    navLinks.style.right = "-200px";
-}
+    var navLinks = document.getElementById("navLinks");
 
-const typed = new Typed('.multiple-text', {
-    strings: ['located in Houma, LA.', 'Friends.', 'Family.'],
-    typedSpeed: 50000,
-    backSpeed: 100,
-    backDelay: 1500,
-    loop: true,
+    function showMenu() {
+        navLinks.style.right = "0";
+    }
 
+    function hideMenu() {
+        navLinks.style.right = "-200px";
+    }
+
+    const typed = new Typed('.multiple-text', {
+        strings: ['Located in Houma, LA.', 'Friends.', 'Family.'],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1500,
+        loop: true
+    });
+
+    document.querySelector('.footer-iconTop a').addEventListener('click', function(e) {
+        e.preventDefault();
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+
+
+
+
+});
+
+
+
+document.getElementById('scrollToTop').addEventListener('click', function(e) {
+    e.preventDefault(); // Prevent the default anchor behavior
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
 });
